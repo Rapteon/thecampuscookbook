d50331cd9109c8c3268ad462cc11783d2fbd697f
Sibukumar Yegamoorthy <2943021Y@student.gla.ac.uk>
2025-03-13 17:15:25 +0000

    Removed models.py from settings folder and fixed some issues in the Recipe and SavedRecipe model.

--- thecampuscookbook/account/models.py
+++ thecampuscookbook/account/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Recipe(models.Model):
+    recipe_id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey(User,on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    origin = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+    ingredients = models.TextField()
+    description = models.TextField()
+    preparation_time = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True)
+    popularity = models.IntegerField(default=0)
+    def __str__(self):
+        return self.title
+
+class Rating(models.Model):
+    rating_id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey(User,on_delete=models.CASCADE)
+    recipe_id = models.ForeignKey(Recipe,on_delete=models.CASCADE)
+    rating = models.SmallIntegerField()
+    def __str__(self):
+        return f"Rating by {self.user.user_name} on {self.recipe.title}"
+
+class SavedRecipes(models.Model):
+    saved_id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey(User,on_delete=models.CASCADE)
+    recipe_id = models.ForeignKey(Recipe,on_delete=models.CASCADE)
+    saved_at = models.DateTimeField(auto_now_add=True, blank=True)
+    def __str__(self):
+        return f"{self.recipe.title} saved by {self.user.user_name}"
--- thecampuscookbook/thecampuscookbook/settings.py
+++ thecampuscookbook/thecampuscookbook/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "login",
     "register",
     "category",
+    "account",
 ]
 
 MIDDLEWARE = [
