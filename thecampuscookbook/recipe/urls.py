a348c5f9c244b12fd1bde564c5a1abfde710dd23
Sibukumar Yegamoorthy <2943021Y@student.gla.ac.uk>
2025-03-26 18:50:48 +0000

    adding remove functionality in saved recipes

--- thecampuscookbook/recipe/urls.py
+++ thecampuscookbook/recipe/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import rate_recipe, save_recipe
+from .views import rate_recipe, save_recipe, remove_recipe
 
 urlpatterns = [
     path("rate/", rate_recipe, name="rate_recipe"),
     path("save/", save_recipe, name="save_recipe"),
+    path('remove/', remove_recipe, name="remove_recipe")
 ]
