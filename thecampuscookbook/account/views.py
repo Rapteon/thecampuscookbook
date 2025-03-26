d102979e6e537bc6a8fca6a996c2e678fa78f2e2
SibukumarYegamoorthy <2943021Y@student.gla.ac.uk>
2025-03-26 12:55:12 +0000

    adding pagination fix saved recipe (#27)

--- thecampuscookbook/account/views.py
+++ thecampuscookbook/account/views.py
@@ -1,23 +1,41 @@
-from django.core.paginator import Paginator
-from django.shortcuts import redirect, render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.shortcuts import redirect,render
 from recipe.models import SavedRecipe
 from django.contrib.auth.decorators import login_required
+from django.db.models import Max
 from recipe.forms import RecipeForm
 from category.models import Category
 
 
 @login_required
 def saved_recipes(request):
-    # Get ALL saved recipes (not filtered by user)
-    saved_recipes_list = SavedRecipe.objects.all().select_related("recipe")
-
-    # Paginate the results - show 6 recipes per page
+    # Get the most recent save ID for each recipe
+    latest_ids = SavedRecipe.objects.filter(
+        user_profile=request.user.userprofile
+    ).values('recipe').annotate(
+        latest_id=Max('id')
+    ).values_list('latest_id', flat=True)
+    
+    # Get complete SavedRecipe objects ordered by save time
+    saved_recipes_list = SavedRecipe.objects.filter(
+        id__in=latest_ids
+    ).select_related('recipe').order_by('-saved_at')
+    
+    # Paginate the results
     paginator = Paginator(saved_recipes_list, 6)
-    page_number = request.GET.get("page")
-    page_obj = paginator.get_page(page_number)
-
-    return render(request, "account/saved-recipes/index.html", {"page_obj": page_obj})
-
+    page_number = request.GET.get('page')
+    
+    try:
+        page_obj = paginator.page(page_number)
+    except PageNotAnInteger:
+        page_obj = paginator.page(1)
+    except EmptyPage:
+        page_obj = paginator.page(paginator.num_pages)
+    
+    return render(request, "account/saved-recipes/index.html", {
+        'page_obj': page_obj,
+        'total_recipes': saved_recipes_list.count()
+    })
 
 @login_required
 def my_recipes(request):
@@ -50,3 +68,4 @@ def add_recipe(request):
 @login_required
 def settings(request):
     return render(request, "account/settings/index.html")
+
