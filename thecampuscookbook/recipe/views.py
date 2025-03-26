b2555c5de58c87d25cebd580114e047283d4f5e8
Rapteon <42870679+Rapteon@users.noreply.github.com>
2025-03-26 17:41:55 +0000

    Added feature to save recipe for a user. (#37)

--- thecampuscookbook/recipe/views.py
+++ thecampuscookbook/recipe/views.py
@@ -1,6 +1,6 @@
 import json
 from django.http import HttpResponseBadRequest, JsonResponse
-from recipe.models import Rating, Recipe
+from recipe.models import Rating, Recipe, SavedRecipe
 from django.db.models import Avg
 
 # Create your views here.
@@ -15,11 +15,9 @@ def rate_recipe(request):
 
             recipe_id = int(data.get("recipeId"))
             if is_already_rated(request, recipe_id):
-                print("Already rated")
                 return JsonResponse({"status": "already rated"}, status=200)
 
             if not is_rating_valid(rating_value):
-                print("Invalid rating value")
                 return JsonResponse(
                     {"status": "rating value out of bounds"}, status=400
                 )
@@ -33,12 +31,10 @@ def rate_recipe(request):
                 recipe.average_rating = Rating.objects.filter(recipe=recipe).aggregate(
                     Avg("rating")
                 )["rating__avg"]
-                print(recipe.average_rating)
                 rating.save()
                 recipe.save()
                 return JsonResponse({"status": "saved"}, status=200)
             except Recipe.DoesNotExist:
-                print("Recipe not found")
                 return JsonResponse({"status": "not found"}, status=404)
         else:
             return HttpResponseBadRequest("Does not accept GET requests.")
@@ -63,6 +59,48 @@ def is_already_rated(request, recipe_id: int):
         )
         print(ratings)
         return len(ratings) > 0
+    except IndexError:
+        return False
+    except Rating.DoesNotExist:
+        return False
+
+
+def save_recipe(request):
+    """Saves a recipe for the user"""
+    if is_ajax_request(request):
+        if request.method == "POST":
+            data = json.load(request)
+
+            recipe_id = int(data.get("recipeId"))
+
+            if is_already_saved(request, recipe_id):
+                return JsonResponse({"status": "already saved"}, status=200)
+
+            try:
+                recipe = Recipe.objects.get(pk=recipe_id)
+                user_profile = request.user.userprofile
+                saved_recipe = SavedRecipe.objects.create(
+                    recipe=recipe, user_profile=user_profile
+                )
+                saved_recipe.save()
+                return JsonResponse({"status": "saved"}, status=200)
+
+            except Recipe.DoesNotExist:
+                return JsonResponse({"status": "not found"}, status=404)
+        else:
+            return HttpResponseBadRequest("Does not accept GET requests.")
+    else:
+        print(request.headers)
+        return HttpResponseBadRequest("Only accepts AJAX requests.")
+
+
+def is_already_saved(request, recipe_id):
+    """Checks if the recipe was already saved by the user"""
+    try:
+        saved_recipe = SavedRecipe.objects.filter(
+            recipe=recipe_id, user_profile=request.user.userprofile
+        )
+        return len(saved_recipe) > 0
     except IndexError:
         return False
     except Rating.DoesNotExist:
