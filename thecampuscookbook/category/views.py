63b05a8850f284790473b795e42674511229ac97
Sibukumar Yegamoorthy <2943021Y@student.gla.ac.uk>
2025-03-26 17:25:46 +0000

    adding category pages pagination code

--- thecampuscookbook/category/views.py
+++ thecampuscookbook/category/views.py
@@ -1,18 +1,27 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
 from .models import Category
 from recipe.models import Recipe
 
-# Create your views here.
-
-
 def show_category(request, category_name_slug):
     context_dict = {}
-
     category = Category.objects.get(slug=category_name_slug)
-
-    recipes = Recipe.objects.filter(category_id=category.id)
-
+    
+    # Get all recipes for this category
+    recipes_list = Recipe.objects.filter(category_id=category.id).order_by('-created_at')
+    
+    # Paginate the results (6 items per page)
+    paginator = Paginator(recipes_list, 6)
+    page_number = request.GET.get('page')
+    
+    try:
+        page_obj = paginator.page(page_number)
+    except PageNotAnInteger:
+        page_obj = paginator.page(1)
+    except EmptyPage:
+        page_obj = paginator.page(paginator.num_pages)
+    
     context_dict["category"] = category
-    context_dict["recipes"] = recipes
-
+    context_dict["page_obj"] = page_obj
+    
     return render(request, "category/index.html", context_dict)
