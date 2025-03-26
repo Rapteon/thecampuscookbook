5733341e810dd0b1cb0ec8b40779c9790a9d585b
Sibukumar Yegamoorthy <2943021Y@student.gla.ac.uk>
2025-03-26 22:20:34 +0000

    my recipe page render fix

--- thecampuscookbook/recipe/admin.py
+++ thecampuscookbook/recipe/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Rating, Recipe
+from .models import Rating, Recipe, SavedRecipe
 
 # Register your models here.
 admin.site.register(Rating)
 admin.site.register(Recipe)
+admin.site.register(SavedRecipe)
+
